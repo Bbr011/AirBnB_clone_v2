852a4707dd1a43e0d300e7261b0d786affe27889
Bbr011 <kahdraouih7@gmail.com>
2024-08-09 14:41:10 +0000

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """flask app with double tabs"""
-from flask import Flask
 
+from flask import Flask
 
 app = Flask(__name__)
 
 """return hello HBNB"""
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 """return HBNB"""
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
