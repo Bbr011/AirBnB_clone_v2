30155ebc9b13477aed7e0e3bdbd65d1c4f1469d6
Bbr011 <kahdraouih7@gmail.com>
2024-08-09 14:38:37 +0000

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
+"""flask app with double tabs"""
 from flask import Flask
 
 
 app = Flask(__name__)
 
+"""return hello HBNB"""
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
+
+"""return HBNB"""
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+"""web app listening on 0.0.0.0, port 5000"""
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
